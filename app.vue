ad41add12cfbf52ce22d25ddb96fd5dafbf0c26d
Leeyonghyeong <tont1990@gmail.com>
2022-11-17 18:03:33 +0900

    nuxt3 init & eslint + prettier + typescript Settings

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtWelcome />
+    <div>
+      sdlkfjaslkjf aslkfjlasfjkasfast;ljasl;ktjaslkjtlksajtljastjlastjla;s
+      <h1>alskfjlaksjtlkasjtlaksjlsakj</h1>
+      <div>aslkjtlasjltj</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+div {
+  padding: 0;
+  margin-top: 0;
+}
+</style>
